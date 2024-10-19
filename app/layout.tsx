8d845730cb099831d0708b200ccf09417047bfbe
kosticivan5 <kosticivan5@gmail.com>
2024-10-19 13:35:27 +0300

    added dark theme only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,40 @@
 import "./globals.css";
+import React from "react";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
